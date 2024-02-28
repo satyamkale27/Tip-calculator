ea7a457f3e8d975436a1cc6b3284c27ed9262657
satyam kale <satyamkale271@gmail.com>
2024-02-28 20:19:30 +0530

    added some html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 function App() {
   return (
     <div>
-      <p>Hello</p>
+      <Form />
     </div>
   );
+
+  function Form() {
+    return (
+      <div>
+        <div>
+          <div>
+            <form className="table">
+              <p>How much was the bill?</p>
+              <input type="text"></input>
+            </form>
+          </div>
+
+          <div>
+            <form className="table">
+              <p>How did you like the service?</p>
+              <input type="text"></input>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
