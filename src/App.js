d0f4c3becf0192e1b0bda0b14108ef1591ce8f5a
satyam kale <satyamkale271@gmail.com>
2024-02-28 20:35:30 +0530

    created basic structure

--- src/App.js
+++ src/App.js
@@ -19,7 +19,24 @@ function App() {
           <div>
             <form className="table">
               <p>How did you like the service?</p>
-              <input type="text"></input>
+              <select>
+                <option value="dissatisfied">Dissatisfied (0%)</option>
+                <option value="okay">it was okay (5%)</option>
+                <option value="good">it was good (10%)</option>
+                <option value="amazing">Absolutely amazing! (20%)</option>
+              </select>
+            </form>
+          </div>
+
+          <div>
+            <form className="table">
+              <p>How did your friend like the service?</p>
+              <select>
+                <option value="dissatisfiedt">Dissatisfied (0%)</option>
+                <option value="okayt">it was okay (5%)</option>
+                <option value="goodt">it was good (10%)</option>
+                <option value="amazingt">Absolutely amazing! (20%)</option>
+              </select>
             </form>
           </div>
         </div>
